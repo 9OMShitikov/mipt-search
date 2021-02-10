767b290f55459ae83c953047c99212588a38e868
Aleksey Ponomarev <aaponomarev@avito.ru>
2021-02-10 17:56:00 +0300

    add cmdparser

---
+++ lib/client/cmdparser.cpp
@@ -0,0 +1,18 @@
+#include "cmdparser.h"
+
+CommandLineParser::CommandLineParser(std::istream& in, std::ostream& out) : in_(in), out_(out) {
+}
+
+bool CommandLineParser::Parse() {
+	// Your code goes here...
+
+	return true;
+}
+
+bool CommandLineParser::IsCompleted() const {
+	return !query_.empty();
+}
+
+std::string CommandLineParser::GetQuery() const {
+	return query_;
+}
